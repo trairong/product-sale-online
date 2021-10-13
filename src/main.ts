73ca646e129f3a2ed4bd01c36fc8c81f796541d2
Trairong <trairong.j@allrightbox.com>
2021-10-13 22:32:43 +0700

    first commit

--- src/main.ts
+++ src/main.ts
@@ -1,29 +1,10 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
-import { SwaggerModule, DocumentBuilder } from '@nestjs/swagger';
-import * as config from 'config';
+
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule, {
-    cors: true,
-  });
-
-  const serverConfig = config.get('server');
-
+  const app = await NestFactory.create(AppModule);
+  // await app.listen(3000);
   app.enableCors();
-
-  app.setGlobalPrefix('api');
-
-  const options = new DocumentBuilder()
-    .setTitle('Cats example')
-    .setDescription('The cats API description')
-    .setVersion('1.0')
-    .addTag('cats')
-    .build();
-  const document = SwaggerModule.createDocument(app, options);
-  SwaggerModule.setup('api', app, document);
-  await app.listen(process.env.PORT || 4000);
-  console.log('======== ENVIROMENT ========:', serverConfig.env);
-  console.log('======== PORT ==============:', serverConfig.port);
-  console.log('======== ORIGIN ==============:', serverConfig.origin);
+  await app.listen(process.env.PORT || 3000);
 }
 bootstrap();
